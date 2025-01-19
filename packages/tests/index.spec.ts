4376fad0f9023b7176bc16333afbc2bfe137223c
Laraib ahmmad <labbuxt@gmail.com>
2025-01-19 23:58:31 +0530

    190125/2

--- packages/tests/index.spec.ts
+++ packages/tests/index.spec.ts
@@ -1,17 +1,25 @@
-import { describe, expect, test } from 'vitest'
-import axios from "axios"
+import { describe, expect, it, test } from 'vitest'
+import axios, { options } from "axios"
 const backenbUrl = 'http://localhost:8080'
-const usernaem1="Laraib";
-const usernaem2="Ahamd";
-const password1="12345";
-const password2="12345";
+const phone_no="Laraib";
+const name="12345";
 describe("Signup endpoints",()=>{
-  test('Double signup doesnt work', async() => {
-    const respomse = await axios.post(`${backenbUrl}/signup`, {
-      username: usernaem1,
-      password: password1
+  it('Double signup doesnt work', async() => {
+    const response1 = await axios.post(`${backenbUrl}/api/v1/signup`, {
+      number: phone_no,
+    });
+    const response2 = await axios.post(`${backenbUrl}/api/v1/signup/verify`, {
+      name: name,
+      otp:"12345",
+    });
+    expect(response1.status).toBe(200);
+    expect(response2.status).toBe(200);
+    
+    expect(async()=>{
+      const response3 = await axios.post(`${backenbUrl}/api/v1/signup`, {
+        number: phone_no,
+      });
+    }).toThrow(); 
     })
-    expect(respomse.status).toBe(200);
-  })
 })
  
